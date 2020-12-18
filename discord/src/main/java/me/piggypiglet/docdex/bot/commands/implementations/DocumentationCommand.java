567f72f6986150e1e1349d3fbc7ec06d26dd9688
PiggyPiglet <noreply@piggypiglet.me>
2020-12-19 01:08:21 +0800

    how did gitkraken miss these

--- discord/src/main/java/me/piggypiglet/docdex/bot/commands/implementations/DocumentationCommand.java
+++ discord/src/main/java/me/piggypiglet/docdex/bot/commands/implementations/DocumentationCommand.java
@@ -1,7 +1,55 @@
 package me.piggypiglet.docdex.bot.commands.implementations;
+
+import com.google.gson.Gson;
+import com.google.inject.Inject;
+import me.piggypiglet.docdex.bot.commands.JDACommand;
+import me.piggypiglet.docdex.config.Config;
+import me.piggypiglet.docdex.documentation.DocumentedObjectSerializer;
+import me.piggypiglet.docdex.documentation.objects.DocumentedObject;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.User;
+import org.jetbrains.annotations.NotNull;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.TimeUnit;
+
 // ------------------------------
 // Copyright (c) PiggyPiglet 2020
 // https://www.piggypiglet.me
 // ------------------------------
-public final class DocumentationCommand {
+public final class DocumentationCommand extends JDACommand {
+    private static final HttpClient CLIENT = HttpClient.newHttpClient();
+    private static final Gson GSON = new Gson();
+
+    private final String url;
+
+    @Inject
+    public DocumentationCommand(@NotNull final Config config) {
+        super("doc", "search a javadoc");
+        this.url = config.getUrl();
+    }
+
+    @Override
+    protected void execute(final @NotNull User user, final @NotNull Message message,
+                           final @NotNull String @NotNull [] args) {
+        final MessageChannel channel = message.getChannel();
+
+        if (args.length < 2) {
+            channel.sendMessage("Incorrect usage. Correct usage is: doc <javadoc> <query>")
+                    .queue(error -> error.delete().queueAfter(15, TimeUnit.SECONDS));
+            return;
+        }
+
+        final HttpRequest request = HttpRequest.newBuilder(URI.create(url + "?javadoc=" + args[0] + "&query=" + args[1].replace("#", "~").replace("%", "-")))
+                .build();
+        CLIENT.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenApply(json -> GSON.fromJson(json, DocumentedObject.class))
+                .thenApply(object -> DocumentedObjectSerializer.toEmbed(args[0], object))
+                .thenAccept(embed -> channel.sendMessage(embed).queue());
+    }
 }
--- discord/src/main/java/me/piggypiglet/docdex/bot/commands/implementations/HelpCommand.java
+++ discord/src/main/java/me/piggypiglet/docdex/bot/commands/implementations/HelpCommand.java
@@ -1,10 +1,15 @@
 package me.piggypiglet.docdex.bot.commands.implementations;
 
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
 import me.piggypiglet.docdex.bot.commands.JDACommand;
+import me.piggypiglet.docdex.bot.commands.JDACommandHandler;
 import me.piggypiglet.docdex.scanning.annotations.Hidden;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
 
 // ------------------------------
 // Copyright (c) PiggyPiglet 2020
@@ -12,14 +17,20 @@ import org.jetbrains.annotations.NotNull;
 // ------------------------------
 @Hidden
 public final class HelpCommand extends JDACommand {
+    private final Set<JDACommand> commands;
 
-
-    public HelpCommand() {
+    @Inject
+    public HelpCommand(@NotNull @Named("jda commands") final Set<JDACommand> commands) {
         super("help", "this page.");
+        this.commands = commands;
     }
 
     @Override
     public void execute(final @NotNull User user, final @NotNull Message message) {
+        final StringBuilder helpMessage = new StringBuilder();
 
+        commands.forEach(command -> helpMessage.append("\n").append(command.getMatch()).append(" - ").append(command.getDescription()));
+
+        message.getChannel().sendMessage(helpMessage.toString()).queue();
     }
 }
