16db50e6a5f92db3fecf44ce68afca45afc7e7f4
PiggyPiglet <noreply@piggypiglet.me>
2020-12-30 17:07:44 +0800

    Added a method of resetting the prefix to it's default

---
+++ discord/src/main/java/me/piggypiglet/docdex/bot/commands/listener/PrefixResetListener.java
@@ -0,0 +1,70 @@
+package me.piggypiglet.docdex.bot.commands.listener;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import me.piggypiglet.docdex.db.dbo.DatabaseObjects;
+import me.piggypiglet.docdex.db.server.Server;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+// ------------------------------
+// Copyright (c) PiggyPiglet 2020
+// https://www.piggypiglet.me
+// ------------------------------
+public final class PrefixResetListener extends ListenerAdapter {
+    private static final ExecutorService EXECUTOR = Executors.newFixedThreadPool(2);
+
+    private final Set<Server> servers;
+    private final Server defaultServer;
+    private final DatabaseObjects databaseObjects;
+
+    @Inject
+    public PrefixResetListener(@NotNull final Set<Server> servers, @NotNull @Named("default") final Server defaultServer,
+                               @NotNull final DatabaseObjects databaseObjects) {
+        this.servers = servers;
+        this.defaultServer = defaultServer;
+        this.databaseObjects = databaseObjects;
+    }
+
+    @Override
+    public void onGuildMessageReceived(@NotNull final GuildMessageReceivedEvent event) {
+        EXECUTOR.submit(() -> {
+            if (event.getAuthor().isBot()) {
+                return;
+            }
+
+            if (!event.getMessage().getContentRaw().startsWith(event.getJDA().getSelfUser().getAsMention().replace("@", "@!") + " resetprefix")) {
+                return;
+            }
+
+            final Server server = servers.stream()
+                    .filter(element -> element.getId().equals(event.getGuild().getId()))
+                    .findAny().orElse(null);
+
+            if (server == null) {
+                return;
+            }
+
+            final Member member = Objects.requireNonNull(event.getMember());
+
+            if (!member.hasPermission(Permission.ADMINISTRATOR) && member.getRoles().stream()
+                    .map(Role::getId)
+                    .noneMatch(server.getRoles()::contains)) {
+                return;
+            }
+
+            server.setPrefix(defaultServer.getPrefix());
+            databaseObjects.save(server);
+            event.getChannel().sendMessage("Successfully set the prefix to " + server.getPrefix()).queue();
+        });
+    }
+}
