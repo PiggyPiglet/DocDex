6a3d1c2d091e6f413d2b6d80f032b5bdbd9842e5
PiggyPiglet <noreply@piggypiglet.me>
2021-01-22 12:31:58 +0800

    removed debug and fixed permission error on emote add

--- discord/src/main/java/me/piggypiglet/docdex/bot/emote/EmoteUtils.java
+++ discord/src/main/java/me/piggypiglet/docdex/bot/emote/EmoteUtils.java
@@ -3,11 +3,9 @@ package me.piggypiglet.docdex.bot.emote;
 import me.piggypiglet.docdex.bot.utils.PermissionUtils;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.exceptions.PermissionException;
-import net.dv8tion.jda.api.requests.RestAction;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
-import java.util.Optional;
 
 // ------------------------------
 // Copyright (c) PiggyPiglet 2020
@@ -19,14 +17,14 @@ public final class EmoteUtils {
     }
 
     public static void addEmote(@NotNull final Message message, @NotNull final EmoteWrapper emote) {
-        Optional.ofNullable(emote.getEmote())
-                .map(message::addReaction)
-                .ifPresentOrElse(RestAction::queue, () -> {
-                    try {
-                        message.addReaction(Objects.requireNonNull(emote.getUnicode())).queue();
-                    } catch (PermissionException exception) {
-                        PermissionUtils.sendPermissionError(message, exception.getPermission());
-                    }
-                });
+        try {
+            if (emote.getEmote() == null) {
+                message.addReaction(Objects.requireNonNull(emote.getUnicode())).queue();
+            } else {
+                message.addReaction(emote.getEmote()).queue();
+            }
+        } catch (PermissionException exception) {
+            PermissionUtils.sendPermissionError(message, exception.getPermission());
+        }
     }
 }
